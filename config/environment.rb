54358a041e0e218d99c18adc580e19fea2be0a52
= <=>
2013-07-03 17:16:39 +0900

    曜日くらいでたほうが良い

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+Dir[File.join(Rails.root, "lib", "extends", "*.rb")].each {|l| require l }
 
 # Initialize the rails application
 Study::Application.initialize!
---
+++ lib/extends/time.rb
@@ -0,0 +1,9 @@
+#coding: utf-8
+class Time
+
+  def wday_jp
+    wdays = ["日", "月", "火", "水", "木", "金", "土"]
+    wdays[self.wday]
+  end
+
+end
