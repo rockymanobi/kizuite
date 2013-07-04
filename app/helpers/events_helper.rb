cafcd84c69a953afab325b5b23f9d85cf1d27a98
rocky_manobi <rocky@rocky-manobi-no-MacBook-Air.local>
2013-07-05 02:56:43 +0900

    calendar display on events.index. display only.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,13 @@
 module EventsHelper
+  def events_exist_at?( events, day )
+    events.select{ |d| day.to_date ==  d.from_date.to_date }.present?
+  end
+
+  def has_event_class( events, day )
+    if events_exist_at?( events, day )
+      "has_event"
+    else
+      ""
+    end
+  end
 end
